1f85a0baf0e81bd1de5f46618293d417ec8bd8f7
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2022-05-04 12:33:32 +0530

    strings updation

--- handlers/inlineMeme.py
+++ handlers/inlineMeme.py
@@ -13,11 +13,11 @@ from utils import imgflip, customFilters
 @Client.on_inline_query(customFilters.isRecentImgflipMeme)
 async def callback(c, m):
 
-    name = m.query.split(" ")[0].lower().strip()
+    name = m.query.split(" ")[0].lower()
 
     # if there is only the memename in the callback, we answer with the meme info
 
-    if m.query.strip() == name:
+    if len(m.query.split(" ")) == 1 and m.query.lower().strip() == name:
 
         memeInfo = imgflip.recent_memes_by_name[name]
 
@@ -85,7 +85,7 @@ async def callback(c, m):
                 results=[
                     InlineQueryResultArticle(
                         title=f"Template: {' '.join(name.split('_')).title()}",
-                        description=f"Required captions: {memeInfo['box_count']}\nGiven captions: {len(text)}",
+                        description=f"Given captions: {len(text)}\nRequired captions: {memeInfo['box_count']}",
                         thumb_url=memeInfo["url"],
                         input_message_content=InputTextMessageContent(
                             message_text=f"**❌ You gave me {len(text)} caption(s) instead of the required {memeInfo['box_count']} for this template.**[­]({memeInfo['url']})",
@@ -104,8 +104,6 @@ async def callback(c, m):
                         ),
                     )
                 ],
-                switch_pm_text="View all templates",
-                switch_pm_parameter="imgflip_templates",
                 cache_time=1,
             )
 
