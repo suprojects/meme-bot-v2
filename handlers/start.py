9c4aa7c030d2991c81690e9171b626c37fc0ecf8
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2022-05-04 12:33:40 +0530

    added start and update db

--- handlers/start.py
+++ handlers/start.py
@@ -1,7 +1,9 @@
 from pyrogram import Client, filters
+from database import botusers
 
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start(c, m):
 
+    await botusers.new(m.from_user)
     await m.reply("Hello")
